260e40c82d09a899720a6002c7cd5b77fe109d7b
Foshati <amirrezafoshati@gmail.com>
2024-12-24 18:03:32 +0330

    fix authMiddleware and add uploadthings

---
+++ app/api/uploadthing/core.ts
@@ -0,0 +1,29 @@
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { Auth } from "@/app/(auth)/_lib/helper/auth";
+
+const f = createUploadthing();
+
+// const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
+
+const handleAuth = () => {
+  const userId = Auth();
+  if (!userId) throw new Error("Unauthorized");
+  return { userId: userId };
+};
+
+// FileRouter for your app, can contain multiple FileRoutes
+export const ourFileRouter = {
+  serverImage: f({
+    image: {
+      maxFileSize: "4MB",
+      maxFileCount: 1,
+    },
+  })
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
--- middleware.ts
+++ middleware.ts
@@ -5,22 +5,29 @@ import { Session } from "./app/(auth)/_lib/auth";
 const authRoutes = ["/sign-in", "/sign-up"];
 const passwordRoutes = ["/reset-password", "/forgot-password"];
 const adminRoutes = ["/admin"];
+const userRoutes = ["/dashboard"];
+const publicRoutes = ["/api/uploadthing"];
 
 export default async function authMiddleware(request: NextRequest) {
   const pathName = request.nextUrl.pathname;
   const isAuthRoute = authRoutes.includes(pathName);
   const isPasswordRoute = passwordRoutes.includes(pathName);
   const isAdminRoute = adminRoutes.includes(pathName);
+  const isUserRoute = userRoutes.includes(pathName);
+  const isPublicRoute = publicRoutes.some(route => pathName.startsWith(route));
+
+  if (isPublicRoute) {
+    return NextResponse.next();
+  }
 
   const { data: session } = await betterFetch<Session>(
     "/api/auth/get-session",
     {
       baseURL: process.env.BETTER_AUTH_URL,
       headers: {
-        //get the cookie from the request
         cookie: request.headers.get("cookie") || "",
       },
-    },
+    }
   );
 
   if (!session) {
@@ -34,13 +41,21 @@ export default async function authMiddleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (isAdminRoute && session.user.role !== "admin") {
+  if (isAdminRoute && session.user.role !== "ADMIN") {
     return NextResponse.redirect(new URL("/", request.url));
   }
+
+  if (isUserRoute && session.user.role !== "USER") {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: [
+    "/((?!_next/static|_next/image|.*\\.png$).*)",
+    "/api/uploadthing/:path*"
+  ],
 };
