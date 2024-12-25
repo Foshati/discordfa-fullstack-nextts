2b6757a3fad61d01919aab33e619349ea73e64bc
Foshati <amirrezafoshati@gmail.com>
2024-12-25 22:56:49 +0330

    fix git

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Auth } from "../(auth)/_lib/helper/auth"
-
-export default async function Page() {
-  const { userId, redirectToSignIn } = await Auth()
-
-  if (!userId) return redirectToSignIn()
-
-  return <h1>Hello, {userId}</h1>
-}
